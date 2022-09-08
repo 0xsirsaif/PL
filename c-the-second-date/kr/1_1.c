f334e1cbe7ee9e77d86570d1af72b35e69582854
saif <sirsaif99@gmail.com>
2022-09-08 06:56:09 +0200

    some with kr

---
+++ c-the-second-date/kr/1_1.c
@@ -0,0 +1,13 @@
+/*
+ * in the book example, there is no function type, which will produce a warning <warning: return type defaults to ‘int’>
+ * return 0; is optional, since the default return value is 0?
+ *
+ * */
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello, World!\n");
+    printf("\\c");
+    return 0;
+}
